57af486c3311fcb892e3ff58cc9a297090a62d85
Darianna Cruz De La Rosa <darianna_cruz@ucne.edu.do>
2021-11-14 14:57:45 -0400

    Creando La carpeta Registro con su rGarante

---
+++ UI/Registros/rGarantes.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Proyecto_Final_AP1.UI.Registros
+{
+    /// <summary>
+    /// Lógica de interacción para rGarantes.xaml
+    /// </summary>
+    public partial class rGarantes : Window
+    {
+        public rGarantes()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Limpiar()
+        {
+            GaranteIDTextBox.Clear();
+            NombreTextBox.Clear();
+            CedulaTextBox.Clear();
+            TelefonoTextBox.Clear();
+            DireccionTextBox.Clear();
+            parentescoTextBox.Clear();
+        }
+
+        private void Nuevo_Click(object sender, RoutedEventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
