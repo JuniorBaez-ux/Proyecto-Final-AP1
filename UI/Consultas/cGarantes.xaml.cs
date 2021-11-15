770c761eb67ff6b90a7ae7921dd59f1816240712
Darianna Cruz De La Rosa <darianna_cruz@ucne.edu.do>
2021-11-15 00:12:31 -0400

    Diseñando la ventana de la  consulta de garantes

---
+++ UI/Consultas/cGarantes.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Proyecto_Final_AP1.UI.Consultas
+{
+    /// <summary>
+    /// Lógica de interacción para cGarantes.xaml
+    /// </summary>
+    public partial class cGarantes : Window
+    {
+        public cGarantes()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
