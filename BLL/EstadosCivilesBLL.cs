c21d72909fdc2f833bdb84edfa67ad9a7856c7cb
JuniorBaez-ux <junior_baez@ucne.edu.do>
2021-11-15 10:50:20 -0400

    Realizando BLL faltantes de algunas entidades.

---
+++ BLL/EstadosCivilesBLL.cs
@@ -0,0 +1,34 @@
+﻿using Proyecto_Final_AP1.DAL;
+using Proyecto_Final_AP1.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Final_AP1.BLL
+{
+    public class EstadosCivilesBLL
+    {
+        public static List<EstadosCiviles> GetList(Expression<Func<EstadosCiviles, bool>> criterio)
+        {
+            List<EstadosCiviles> lista = new List<EstadosCiviles>();
+            Contexto contexto = new Contexto();
+            try
+            {
+                lista = contexto.EstadosCiviles.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+    }
+}
---
+++ BLL/ViviendasBLL.cs
@@ -0,0 +1,34 @@
+﻿using Proyecto_Final_AP1.DAL;
+using Proyecto_Final_AP1.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Final_AP1.BLL
+{
+    public class ViviendasBLL
+    {
+        public static List<Viviendas> GetList(Expression<Func<Viviendas, bool>> criterio)
+        {
+            List<Viviendas> lista = new List<Viviendas>();
+            Contexto contexto = new Contexto();
+            try
+            {
+                lista = contexto.Viviendas.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+    }
+}
