72f2424ca4fab6a9a5d0901f88657cb3a7e8f8c5
JuniorBaez-ux <junior_baez@ucne.edu.do>
2021-11-19 11:35:22 -0400

    Modificando la entidad Negocios.

--- Entidades/Negocios.cs
+++ Entidades/Negocios.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +14,12 @@ namespace Proyecto_Final_AP1.Entidades
         public int NegocioId { get; set; }
         public string Nombre { get; set; }
         public string Telefono { get; set; }
-        public string Actividad { get; set; }
         public string Direccion { get; set; }
+
+        [ForeignKey("TipoNegocioId")]
+        public virtual TipoNegocios TipoNegocios { get; set; }
+
+        [ForeignKey("UsuarioId")]
+        public virtual Usuarios Usuarios { get; set; }
     }
 }
