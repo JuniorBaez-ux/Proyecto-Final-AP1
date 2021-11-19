57f4f13535d715c50e1ae9bce96332b852baab4a
JuniorBaez-ux <junior_baez@ucne.edu.do>
2021-11-19 11:14:37 -0400

    Creacion de la entidad Moras.

---
+++ Entidades/Moras.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Final_AP1.Entidades
+{
+    public class Moras
+    {
+        [Key]
+        public int MoraId { get; set; }
+        public DateTime Fecha { get; set; }
+        public double Monto { get; set; }
+
+        [ForeignKey("PrestamoId")]
+        public virtual Prestamos Prestamos { get; set; }
+
+        [ForeignKey("UsuarioId")]
+        public virtual Usuarios Usuarios { get; set; }
+    }
+}
