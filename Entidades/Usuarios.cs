516465704212f3603e842939195804cab66b7419
JuniorBaez-ux <junior_baez@ucne.edu.do>
2021-11-19 11:05:17 -0400

    Creando la entidad de Usuarios.

---
+++ Entidades/Usuarios.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Final_AP1.Entidades
+{
+    public class Usuarios
+    {
+        [Key]
+        public int UsuarioId { get; set; }
+        public string Nombres { get; set; }
+        public string Email { get; set; }
+        public bool Activo { get; set; }
+
+        [ForeignKey("RolId")]
+        public List<Roles> Detalle { get; set; }
+
+    }
+}
