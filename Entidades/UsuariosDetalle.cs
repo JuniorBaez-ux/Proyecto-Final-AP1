4892759ecc66cc59b4486547ee825b080b746430
JuniorBaez-ux <junior_baez@ucne.edu.do>
2021-11-19 11:09:41 -0400

    Creando la entidad UsuariosDetalle.

---
+++ Entidades/UsuariosDetalle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Final_AP1.Entidades
+{
+    public class UsuariosDetalle
+    {
+        [Key]
+        public int PermisoId { get; set; }
+
+
+        [ForeignKey("RolId")]
+        public List<Roles> Roles { get; set; }
+
+
+        [ForeignKey("UsuarioId")]
+        public List<Usuarios> Usuarios { get; set; }
+    }
+}
