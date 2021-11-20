49a59e9b1f05c825849e6e40597c97b25eeb0c00
JuniorBaez-ux <junior_baez@ucne.edu.do>
2021-11-20 15:54:38 -0400

    Agregando parte de la funcionalidad del registro de prestamo.

--- UI/Registros/rPrestamos.xaml.cs
+++ UI/Registros/rPrestamos.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Proyecto_Final_AP1.BLL;
+using Proyecto_Final_AP1.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,29 +21,26 @@ namespace Proyecto_Final_AP1.UI.Registros
     /// </summary>
     public partial class rPrestamos : Window
     {
+        private Prestamos prestamos = new Prestamos();
         public rPrestamos()
         {
             InitializeComponent();
+            this.DataContext = prestamos;
         }
 
         private void BuscarIDButton_Click(object sender, RoutedEventArgs e)
         {
+            var Prestamos = PrestamosBLL.Buscar(Utilidades.ToInt(IdTextBox.Text));
 
-        }
-
-        private void NuevoButton_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void GuardarButton_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void EliminarButton_Click(object sender, RoutedEventArgs e)
-        {
-
+            if (Prestamos != null)
+            {
+                this.prestamos = Prestamos;
+            }
+            else
+            {
+                this.prestamos = new Prestamos();
+            }
+            Cargar();
         }
 
         private void CalcularCuotasButton_Click(object sender, RoutedEventArgs e)
@@ -52,6 +51,98 @@ namespace Proyecto_Final_AP1.UI.Registros
         private void RemoverFila_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void NuevoButton_Click(object sender, RoutedEventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Validar())
+            {
+                return;
+            }
+
+            llenarProyec();
+
+            var paso = PrestamosBLL.Guardar(this.prestamos);
+
+            if (paso)
+            {
+                Limpiar();
+                MessageBox.Show("Su prestamo ha sido guardado correctamente");
+            }
+            else
+            {
+                MessageBox.Show("Su proyecto no ha podido ser almacenado...");
+            }
+        }
+
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+            Prestamos existe = PrestamosBLL.Buscar(this.prestamos.PrestamoId);
+
+            if (PrestamosBLL.Eliminar(this.prestamos.PrestamoId))
+            {
+                Limpiar();
+                MessageBox.Show("Su Prestamo ha sido eliminado con exito");
+            }
+            else
+            {
+                MessageBox.Show("No fue posible eliminarlo");
+            }
+        }
+
+        private void Cargar()
+        {
+            this.DetalleDataGrid.ItemsSource = null;
+            this.DetalleDataGrid.ItemsSource = this.prestamos.Detalle;
+            this.DataContext = null;
+            this.DataContext = this.prestamos;
+        }
+
+        private void Limpiar()
+        {
+            prestamos = new Prestamos();
+            Cargar();
+        }
+
+        private bool Validar()
+        {
+            bool esValido = true;
+
+            if (IdTextBox.Text.Length == 0)
+            {
+                esValido = false;
+                MessageBox.Show("El campo Prestamos Id no puede quedar vacio");
+            }
+            if (MontoTextBox.Text.Length == 0)
+            {
+                esValido = false;
+                MessageBox.Show("El campo Monto no puede quedar vacio");
+            }
+            if (CuotasTextBox.Text.Length == 0)
+            {
+                esValido = false;
+                MessageBox.Show("El campo Cuotas no puede quedar vacio");
+            }
+            if (InteresTextBox.Text.Length == 0)
+            {
+                esValido = false;
+                MessageBox.Show("El campo Interes no puede quedar vacio");
+            }
+            return esValido;
+        }
+
+        private void llenarProyec()
+        {
+            prestamos.PrestamoId = Utilidades.ToInt(IdTextBox.Text);
+            prestamos.Monto = Utilidades.ToInt(MontoTextBox.Text);
+            prestamos.Cuotas = Utilidades.ToInt(CuotasTextBox.Text);
+            prestamos.Interes = Utilidades.ToInt(InteresTextBox.Text);
+            prestamos.Balance = Utilidades.ToInt(BalanceTextBox.Text);
         }
     }
 }
