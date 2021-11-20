26b6494a5d03a249ab4b1d64e802933a3996f370
JuniorBaez-ux <junior_baez@ucne.edu.do>
2021-11-19 21:22:11 -0400

    Creando la BLL de sexos.

--- BLL/SexosBLL.cs
+++ BLL/SexosBLL.cs
@@ -1,4 +1,5 @@
-﻿using Proyecto_Final_AP1.DAL;
+﻿using Microsoft.EntityFrameworkCore;
+using Proyecto_Final_AP1.DAL;
 using Proyecto_Final_AP1.Entidades;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,120 @@ namespace Proyecto_Final_AP1.BLL
 {
     public class SexosBLL
     {
+         public static bool Existe(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool encontrado = false;
+            try
+            {
+                encontrado = contexto.Sexos.Any(e => e.SexoId == id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return encontrado;
+        }
+        private static bool Insertar(Sexos sexos)
+        {
+            Contexto contexto = new Contexto();
+            bool paso = false;
+
+            try
+            {
+                contexto.Add(sexos);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        private static bool Modificar(Sexos sexos)
+        {
+            Contexto contexto = new Contexto();
+            bool paso = false;
+
+            try
+            {
+                contexto.Entry(sexos).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Guardar(Sexos sexos)
+        {
+            if (!Existe(sexos.SexoId))
+                return Insertar(sexos);
+            else
+                return Modificar(sexos);
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                var Sexos = contexto.Sexos.Find(id);
+                if (Sexos != null)
+                {
+                    contexto.Entry(Sexos).State = EntityState.Deleted;
+                    paso = contexto.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static Sexos Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Sexos sexos;
+
+            try
+            {
+                sexos = contexto.Sexos.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return sexos;
+        }
         public static List<Sexos> GetList(Expression<Func<Sexos, bool>> criterio)
         {
             List<Sexos> lista = new List<Sexos>();
