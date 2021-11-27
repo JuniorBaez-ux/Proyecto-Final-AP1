97221df5c7806d4dc8aadb8bccf99564aea29e28
JuniorBaez-ux <junior_baez@ucne.edu.do>
2021-11-27 13:52:30 -0400

    Añadiendo funcionalidades a Utilidades.

--- BLL/Utilidades.cs
+++ BLL/Utilidades.cs
@@ -7,13 +7,37 @@ using System.Threading.Tasks;
 namespace Proyecto_Final_AP1.BLL
 {
 
-    public class Utilidades
+    public static class Utilidades
     {
-        public static int ToInt(string valor)
+        public static int ToInt(this string valor)
         {
             int retorno;
 
             int.TryParse(valor, out retorno);
+
+            return retorno;
+        }
+        public static Decimal ToDecimal(this string valor)
+        {
+            Decimal retorno;
+
+            Decimal.TryParse(valor, out retorno);
+
+            return retorno;
+        }
+        public static double ToDouble(this object valor)
+        {
+            double retorno;
+
+            double.TryParse(valor.ToString(), out retorno);
+
+            return retorno;
+        }
+        public static decimal Pow(object x,object y)
+        {
+            decimal retorno;
+
+            retorno = Math.Pow(x.ToDouble(), y.ToDouble()).ToString().ToDecimal();
 
             return retorno;
         }
