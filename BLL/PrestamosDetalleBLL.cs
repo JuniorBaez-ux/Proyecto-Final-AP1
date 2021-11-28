6871f3d64491211ef26c3754c2955289c95e5b65
JuniorBaez-ux <junior_baez@ucne.edu.do>
2021-11-27 20:32:00 -0400

    Creando PrestamosDetalleBLL..

---
+++ BLL/PrestamosDetalleBLL.cs
@@ -0,0 +1,33 @@
+﻿using Proyecto_Final_AP1.DAL;
+using Proyecto_Final_AP1.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Final_AP1.BLL
+{
+    public class PrestamosDetalleBLL
+    {
+        public static PrestamosDetalle Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            PrestamosDetalle prestamosdetalle;
+            try
+            {
+                prestamosdetalle = contexto.PrestamosDetalle.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return prestamosdetalle;
+        }
+    }
+}
