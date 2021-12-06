d775790e0b6ae4f64c317add7ef7ffeace07c4ab
Darianna Cruz De La Rosa <darianna_cruz@ucne.edu.do>
2021-12-06 15:46:21 -0400

    Añadiendo propiedad a DireccionTextBox a rGarantes

--- UI/Registros/rGarantes.xaml.cs
+++ UI/Registros/rGarantes.xaml.cs
@@ -182,5 +182,11 @@ namespace Proyecto_Final_AP1.UI.Registros
             Regex regex = new Regex("[^0-9-]+");
             e.Handled = regex.IsMatch(e.Text);
         }
+
+        private void DireccionTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^a-zA-Z0-9#.,/]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
     }
 }
