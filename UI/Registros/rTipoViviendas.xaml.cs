b1e6a84d4855c6a23f7d28f41fc975ca3ac38708
Darianna Cruz De La Rosa <darianna_cruz@ucne.edu.do>
2021-12-06 15:28:20 -0400

    Añadiendo propiedad a Idtextbox de rTipoviviendas

--- UI/Registros/rTipoViviendas.xaml.cs
+++ UI/Registros/rTipoViviendas.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -120,6 +121,12 @@ namespace Proyecto_Final_AP1.UI.Registros
             {
                 MessageBox.Show("No fue posible eliminarlo");
             }
+        }
+
+        private void TipoViviendasIdTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
